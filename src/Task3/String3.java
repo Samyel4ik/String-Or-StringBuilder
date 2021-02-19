cf130888b636ba634d52e4babc900a49b6b1f9b1
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-02-19 13:25:03 +0300

    T4

--- src/Task3/String3.java
+++ src/Task3/String3.java
@@ -1,5 +1,21 @@
 package Task3;
 
 public class String3 {
-    //
+    // Проверить, является ли заданное слово палиндромом/
+    public static void main(String[] args) {
+        String str = "jasartvfaj";
+
+        StringBuilder str1 = new StringBuilder(str);
+        str1.reverse();
+        String str3 = str1.toString();
+
+        if (str.equalsIgnoreCase(str3)) {
+            System.out.println("Это слово является палиандром");
+        } else {
+            System.out.println("Это слово не является палиандром");
+        }
+        //System.out.println(str.equalsIgnoreCase(str3));  // сразу выведет true false без учета регистра
+        //System.out.println(str.equals(str3)); // с учетом регистра символов сравнит слово на полиандром.
+
+    }
 }
