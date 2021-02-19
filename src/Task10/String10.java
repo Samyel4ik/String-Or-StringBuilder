fb63749409271a0db6bed04691e73f106a567eac
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-02-19 22:11:27 +0300

    T8T10

--- src/Task10/String10.java
+++ src/Task10/String10.java
@@ -3,4 +3,22 @@ package Task10;
 public class String10 {
     //Строка X состоит из нескольких предложений, каждое из которых кончается точкой, восклицательным или вопросительным знаком.
     // Определить количество предложений в строке X.
+    public static void main(String[] args) {
+        String text = "The world is beautiful. When there is no war in the world! Java?";
+        char [] array = text.toCharArray();
+
+        int t = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]=='.'){
+                t++;
+            }
+            if (array[i]=='!'){
+                t++;
+            }
+            if (array[i]=='?'){
+                t++;
+            }
+        }
+        System.out.println("В строке:" + t + "предложений");
+    }
 }
--- src/Task8/String8.java
+++ src/Task8/String8.java
@@ -3,4 +3,20 @@ package Task8;
 public class String8 {
     //Вводится строка слов, разделенных пробелами.
     // Найти самое длинное слово и вывести его на экран. Случай, когда самых длинных слов может быть несколько, не обрабатывать.
+    public static void main(String[] args) {
+        String text = "the world is beautiful when there is no war in the world";
+        char [] array = text.toCharArray();
+
+        StringBuilder max = new StringBuilder();
+
+        int t = 0;
+        int m = 0;
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i]!=' '){
+                t++;
+
+            }
+        }
+    }
 }
