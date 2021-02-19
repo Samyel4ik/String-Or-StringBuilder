4b42b7e8820569b7f802d56b3ca53dedba553da1
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-02-19 21:30:21 +0300

    T7

--- src/Task7/String7.java
+++ src/Task7/String7.java
@@ -5,13 +5,36 @@ public class String7 {
     // Например, если было введено "abc cde def", то должно быть выведено "abcdef".
     public static void main(String[] args) {
         String str = "abc cde def";
-        char [] array =str.toCharArray();
+        char[] array = str.toCharArray();
         StringBuilder sss = new StringBuilder();
 
-        for (int i = 0; i < array.length ; i++) {
-            
 
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != ' ') {
+                sss.append(array[i]);
+            }
         }
-        System.out.println(sss.toString());
+
+        String str1 = sss.toString();
+        char[] array1 = str1.toCharArray();
+        StringBuilder sss1 = new StringBuilder();
+
+        boolean t;                                          // флажок проверки повторяемости тип
+        for (int i = 0; i < array1.length; i++) {
+            t = false;
+            for (int j = i + 1; j < array1.length; j++) {
+
+                if (array1[i] == array1[j]) {
+                    t = true;
+                    break;
+                }
+            }
+            if (!t) {
+                sss1.append(array1[i]);
+            }
+        }
+        System.out.println(sss1.toString());
     }
+
 }
+
