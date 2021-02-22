d848c6ef9bbb69fd0299b0cba987ace9972c9fb4
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-02-22 12:59:13 +0300

    T6

--- src/Task6/String6.java
+++ src/Task6/String6.java
@@ -4,11 +4,10 @@ public class String6 {
     //Из заданной строки получить новую, повторив каждый символ дважды.
     public static void main(String[] args) {
         String str = "Java";
-        char [] array = str.toCharArray();
         String str1 = "";
 
-        for (int i = 0; i < array.length; i++) {
-            str1+= str.substring(i,i+1) + str.substring(i, i+1);//создаем новую строку , возвращая из исходной с помощью метода букву + еще раз
+        for (int i = 0; i < str.length(); i++) {
+            str1 += str.substring(i, i + 1) + str.substring(i, i + 1);//создаем новую строку , возвращая из исходной с помощью метода букву + еще раз
         }
         System.out.println(str1);
     }
