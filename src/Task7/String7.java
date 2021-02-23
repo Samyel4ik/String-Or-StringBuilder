8032f889fc273a4ae6c49f70255522da5d7b2aea
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-02-23 10:59:29 +0300

    T7

--- src/Task7/String7.java
+++ src/Task7/String7.java
@@ -6,35 +6,13 @@ public class String7 {
     public static void main(String[] args) {
         String str = "abc cde def";
         char[] array = str.toCharArray();
-        StringBuilder sss = new StringBuilder();
-
+        StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < array.length; i++) {
-            if (array[i] != ' ') {
-                sss.append(array[i]);
+            if (!result.toString().contains(String.valueOf(array[i])) && array[i] != ' ') {
+                result.append(array[i]);
             }
         }
-
-        String str1 = sss.toString();
-        char[] array1 = str1.toCharArray();
-        StringBuilder sss1 = new StringBuilder();
-
-        boolean t;                                          // флажок проверки повторяемости тип
-        for (int i = 0; i < array1.length; i++) {
-            t = false;
-            for (int j = i + 1; j < array1.length; j++) {
-
-                if (array1[i] == array1[j]) {
-                    t = true;
-                    break;
-                }
-            }
-            if (!t) {
-                sss1.append(array1[i]);
-            }
-        }
-        System.out.println(sss1.toString());
+        System.out.println(result.toString());
     }
-
 }
-
